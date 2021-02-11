ab1daf10bded4b430d7045e0e5a9a52f7731c94a
David Coronado <77657105+dcoronado69@users.noreply.github.com>
2021-02-10 20:43:16 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ print("Oil change -- $35\nTire rotation -- $19\nCar wash -- $7\nCar wax -- $12\n
 service1 = input("Select first service: \n")
 service2 = input("Select second service: ")
 if service1 == "Oil change":
-   service1_cost = 35
+    service1_cost = 35
 elif service1 == "Tire rotation":
-   service1_cost = 19
+    service1_cost = 19
 elif service1 == "Car wash":
-   service1_cost = 7
+    service1_cost = 7
 elif service1 == "Car wax":
-   service1_cost = 12
+    service1_cost = 12
 elif service1 == "-":
-   service1 = "No service"
-   service1_cost = 0
+    service1 = "No service"
+    service1_cost = 0
 if service2 == "Oil change":
-   service2_cost = 35
+    service2_cost = 35
 elif service2 == "Tire rotation":
-   service2_cost = 19
+    service2_cost = 19
 elif service2 == "Car wash":
-   service2_cost = 7
+    service2_cost = 7
 elif service2 == "Car wax":
-   service2_cost = 12
+    service2_cost = 12
 elif service2 == "-":
-   service2 = "No service"
-   service2_cost = 0
+    service2 = "No service"
+    service2_cost = 0
 print("- - -")
 print("Davy's auto shop invoice")
 print("Service 1: " + service1 + " costs $" + str(service1_cost))
